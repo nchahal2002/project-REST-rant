72f508a84741b29c72db95da6ce0e748df641cf9
Nihal Chahal <nchahal2002@gmail.com>
2023-10-24 19:47:08 -0500

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
-
-
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -17,4 +19,5 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
+// Listen for Connections
 app.listen(process.env.PORT)
