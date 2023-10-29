0d12789c10e838f12c7f3d8bb2a87980e188f362
Nihal Chahal <nchahal2002@gmail.com>
2023-10-29 11:28:02 -0500

    Adding index page to places

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
+router.get('/', (req, res) => {
+  res.render('places/index', { places })
+})
+
 // GET /places/new
 router.get('/new', (req, res) => {
   res.render('places/new')
@@ -20,6 +24,20 @@ router.post('/', (req, res) => {
   }
   places.push(req.body)
   res.redirect('/places')
+})
+
+// SHOW /places
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if  (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
 })
 
 module.exports = router
--- models/places.js
+++ models/places.js
@@ -3,11 +3,11 @@ module.exports = [{
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
-    pic: '/images/h-thai-ml-tables.jpg'
+    pic: '/images/h-thai-ml-tables.png'
 }, {
     name: 'Coding Cat Cafe',
     city: 'Phoenix',
     state: 'AZ',
     cuisines: 'Coffee, Bakery',
-    pic: '/images/coffee-cat.jpg'
+    pic: '/images/coffee-cat.png'
 }]
