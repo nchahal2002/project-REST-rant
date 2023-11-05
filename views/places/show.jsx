2854adf1ac8dcd806f7ca323177f52c29c30a220
Nihal Chahal <nchahal2002@gmail.com>
2023-11-05 10:35:56 -0600

    Made changes to file

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,15 +6,24 @@ function show (data) {
         <Def>
             <main>
                 <div className="container">
-                    <div class="row align-items-start">
+                    <div className="row align-items-start">
                         <div className="col">
                             <img src={ data.place.pic } alt={ data.place.name } />
+                            <h3>
+                                Located in {data.place.city}, {data.place.state}
+                            </h3>
                         </div>
                         <div className="col">
                             <h1>{ data.place.name }</h1>
                             <h2>Rating</h2>
                             <p>Not Rated</p>
                             <h2>Description</h2>
+                            <h3>
+                                {data.place.showEstablished()}
+                            </h3>
+                            <h4>
+                                Serving {data.place.cuisines}
+                            </h4>
                             <p>Located in { data.place.city }, { data.place.state } and serving { data.place.cuisines }</p>
                         </div>
                         <div className="row">
