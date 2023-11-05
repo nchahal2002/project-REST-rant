e5bc7b820ce49f1de085b8a1b46f1ca868bacfe6
Nihal Chahal <nchahal2002@gmail.com>
2023-11-05 11:28:49 -0600

    Made changes to errors in places and show and new files

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,13 +7,13 @@ function show (data) {
             <main>
                 <div className="container">
                     <div className="row align-items-start">
-                        <div className="col">
+                        <div className="col-sm-6">
                             <img src={ data.place.pic } alt={ data.place.name } />
                             <h3>
                                 Located in {data.place.city}, {data.place.state}
                             </h3>
                         </div>
-                        <div className="col">
+                        <div className="col-sm-6">
                             <h1>{ data.place.name }</h1>
                             <h2>Rating</h2>
                             <p>Not Rated</p>
